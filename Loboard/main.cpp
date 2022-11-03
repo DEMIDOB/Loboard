e62cd07e08fb0bb91ec77a7eef71ecdd34b0ae32
DEMIDOB <5OxymA16TytlikuGIT>
2022-11-03 16:35:02 +0100

    Initial Commit

---
+++ Loboard/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Loboard
+//
+//  Created by Даня Демидов on 03.11.22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
